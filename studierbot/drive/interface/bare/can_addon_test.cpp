6e9503ca1c34fbdc77cd306362a84fad3a80538b
BuildTools <not.provided@yet>
2018-12-02 10:50:52 +0100

    added class for add-on shield

---
+++ studierbot/drive/interface/bare/can_addon_test.cpp
@@ -0,0 +1,51 @@
+/**
+ * @author Stefan May
+ * @date 01.12.2018
+ * @brief Test program for CAN interface of add-on board
+ */
+#include "AddonShieldCAN.h"
+#include <unistd.h>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+  SocketCAN can(std::string("slcan0"));
+  can.startListener();
+
+  AddonShieldCAN shield(&can);
+
+  float servoFrequency = 330.f;
+  float uLower = 500.f;
+  float uUpper = 2500.f;
+  shield.setPWMFrequency((int)servoFrequency);
+  shield.enable(2);
+  shield.enable(3);
+  shield.enable(4);
+
+  // Voltage threshold (automatic switch-off)
+  shield.setThreshold(2, 8.f);
+  shield.setThreshold(3, 13.f);
+  shield.setThreshold(4, 6.f);
+
+  int lowerPoint = (int)(uLower / ((1.f / servoFrequency) * 1000000.f) * 100.f - 0.5f);
+  int upperPoint = (int)(uUpper / ((1.f / servoFrequency) * 1000000.f) * 100.f + 0.5f);
+  shield.setPulseWidth(4, lowerPoint);
+  usleep(2000000);
+
+  for(int i=lowerPoint; i<upperPoint;)
+  {
+    shield.setPulseWidth(4, i);
+    if(shield.waitForSync(100))
+    {
+      i++;
+      cout << "Pulse-width: " << i << ", Voltage: " << shield.getVoltage() << " V" << endl;
+      usleep(100000);
+    }
+  }
+
+  can.stopListener();
+
+}
