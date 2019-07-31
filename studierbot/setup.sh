bcf9b9b23bdbd782e9dc0ab80d7613c972febcd3
Stefan May <stefan.may@th-nuernberg.de>
2019-07-31 23:23:52 +0200

    removed obsolete file

--- studierbot/setup.sh
+++
@@ -1,9 +0,0 @@
-sudo echo "18" > /sys/class/gpio/export
-sudo echo "23" > /sys/class/gpio/export
-sudo echo "24" > /sys/class/gpio/export
-sudo echo "25" > /sys/class/gpio/export
-
-sudo echo "in" > /sys/class/gpio/gpio18/direction
-sudo echo "in" > /sys/class/gpio/gpio23/direction
-sudo echo "in" > /sys/class/gpio/gpio24/direction
-sudo echo "in" > /sys/class/gpio/gpio25/direction
